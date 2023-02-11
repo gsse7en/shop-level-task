17163cb3ae69f77d9a6ffe182af934f03ae6634f
gsse7en <latyshev.fedor@gmail.com>
2023-02-12 01:10:11 +0200

    add shop with shop popup to screen

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIController : MonoBehaviour
+    {
+        [SerializeField]
+        private Button m_button;
+        [SerializeField]
+        private GameObject m_ShopScreen;
+
+        #region Lifecycle
+        void Start()
+        {
+            m_ShopScreen.SetActive(true);
+        }
+
+        void Update()
+        {
+
+        }
+        #endregion
+
+        #region Public
+        public void CloseShop()
+        {
+            m_ShopScreen.SetActive(false);
+        }
+
+        public void Buy()
+        {
+            Debug.Log("BuyButton Pressed");
+        }
+
+        public void Sell()
+        {
+            Debug.Log("SellButton Pressed");
+        }
+        #endregion
+    }
+}
